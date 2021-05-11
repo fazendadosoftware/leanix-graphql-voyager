f1f958f32301ae0c767470f838d4f6479f8f8e1c
Paulo dos Santos <paulo@motovalor.com>
2021-05-11 15:25:35 -0300

    current state

--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ async function introspectionProvider(query) {
   return { data }
 }
 
+// https://app.leanix.net/services/pathfinder/v1/reports/files/d0a0a044-0d97-4d8b-9e2d-c93a0b17a1ba/02aade5e-62de-44be-aa47-ffb6c93032f8/assets/js/voyager.worker.js
 ReactDOM.render(
-  <Voyager introspection={introspectionProvider} workerURI={'/assets/js/voyager.worker.js'} />,
+  <Voyager
+    introspection={introspectionProvider}
+    workerURI={'/assets/js/voyager.worker.js'} />,
   document.getElementById('report'),
 )
